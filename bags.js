25e1b651e62b9c2077d6661ebb2eff07cccdb5d1
shehab waleed <shehabwaleed2010@gmail.com>
2022-05-09 22:45:23 +0200

    bags page v1.0

---
+++ bags.js
@@ -0,0 +1,12 @@
+alert("Welcome to our shop");
+
+const chk = document.getElementById('chk');
+
+chk.addEventListener('click', () => {
+	document.body.classList.toggle('dark');
+});
+
+function myFunction() {
+	alert("Done ! , Thank you for buying from us");
+  }
+
